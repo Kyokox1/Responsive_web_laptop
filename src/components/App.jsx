c55e7e22a92b539322e920a3e3f0ac8058f2e4eb
kyoko <kevin.kbr123@gmail.com>
2022-05-08 13:58:09 -0400

    Terminando el Header.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-import "../styles/"
+
+import logo from "../../public/images/logo.svg"
+import cart from "../../public/images/cart.svg"
+
+import "../styles/app.scss"
 
 function App() {
 	return <>
-		<h1>Hola Mundo</h1>
+		<header>
+			<div className="logo" >
+				<a href="/"  className="logo__link" >
+					<img src={logo} alt="logo" />
+					<p>Three Dots</p>
+				</a>
+			</div>
+			<nav className="navbar" >
+				<ul className="navbar__links" >
+					<li> <a href="/" className="navbar__link" >Specs	</a> </li>
+					<li> <a href="/" className="navbar__link" >Products	</a> </li>
+					<li> <a href="/" className="navbar__link" >Contact	</a> </li>
+				</ul>
+			</nav>
+			<div className="cart" >
+				<img  className="cart__img" src={cart} alt="cart" />
+			</div>
+		</header>
 	</>;
 }
 
