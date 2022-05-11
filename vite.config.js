b208b036a45d27282761fd1870d177f5af1bd1ca
kyoko <kevin.kbr123@gmail.com>
2022-05-11 13:02:17 -0400

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Responsive_web_laptop/",
+  // base:"/Responsive_web_laptop/",
   plugins: [react()]
 })
